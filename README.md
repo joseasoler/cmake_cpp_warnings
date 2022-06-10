5f7a767432419c86f6e8620b5f8db229ec47e490
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2022-06-10 11:43:43 +0200

    Port some improvements and extra warnings into this project.

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # CMake C++ Warnings
 
-A CMake module that defines an extensive set of reasonable warnings to be used in a project. The module supports GCC, Clang and Microsoft Visual C++. Warnings that are only supported in newer compiler versions will not be enabled in older versions.
+A CMake module that defines an extensive set of reasonable warnings to be used in modern C++ projects. The module supports GCC, Clang and Microsoft Visual C++. Warnings that are only supported in newer compiler versions will not be enabled in older versions. The documentation of each enabled warning includes a short summary about its purpose.
 
 ## Getting Started
 
 Download the [cpp_warnings.cmake](https://raw.githubusercontent.com/joseasoler/cmake_cpp_warnings/master/cmake/cpp_warnings.cmake) CMake module and add it to your project. In your CMakeLists.txt file, you should include it after your project is defined and the language is set to C++. To use the warnings in a target, you can use target_compile_options.
 
 `target_compile_options(targetName PRIVATE ${PROJECT_WARNING_FLAGS})`
+
+## Recommended usage
+
+It is easier to develop code without warnings from scratch than to refactor it later. So initially the set of enabled warnings should be as extensive as possible. Specific warnings can be disabled later in the development process if it is decided that it makes sense for the project or if they trigger false positives.
 
 ## License
 
@@ -15,3 +19,4 @@ This project is licensed under the MIT License - see the [LICENSE](LICENSE) file
 ## Acknowledgments
 * **[Craig Scott](https://crascit.com/)** - [Professional CMake](https://crascit.com/professional-cmake/)
 * **[Jason Turner](https://github.com/lefticus)** - [C++ Best Practices](https://lefticus.gitbooks.io/cpp-best-practices/)
+* **[Peter Kolbus](https://github.com/pkolbus)** - [Compiler Warnings](https://github.com/pkolbus/compiler-warnings)
--- cmake/cpp_warnings.cmake
+++ cmake/cpp_warnings.cmake
@@ -8,72 +8,104 @@ include_guard(GLOBAL)
 option(WARNINGS_AS_ERRORS "Targets using PROJECT_WARNING_FLAGS will treat warnings as errors." OFF)
 
 if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-  option(CLANG_ENABLE_ALL_WARNINGS "PROJECT_WARNING_FLAGS will include all available warnings when using Clang" OFF)
+  option(CLANG_ENABLE_ALL_WARNINGS "PROJECT_WARNING_FLAGS will add all warnings (except C++98 compatibility ones) when using Clang" OFF)
 endif ()
 
 # When using MSVC in CMake 3.14 and below, /W3 is added to CMAKE_CXX_FLAGS by default.
 if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC" AND "${CMAKE_CXX_FLAGS}" MATCHES "/W3")
-  message("-- Disabling /W3 flag added by default by CMake. See policy CMP0092.")
+  message(STATUS "Disabling /W3 flag added by default by CMake. See policy CMP0092.")
   string(REGEX REPLACE "/W3 " "" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
 endif ()
 
 
-message("-- Configuring PROJECT_WARNING_FLAGS.")
+message(STATUS "Configuring PROJECT_WARNING_FLAGS.")
 # Generate a warning flags list.
 set(PROJECT_WARNING_FLAGS)
 
 if (CLANG_ENABLE_ALL_WARNINGS)
   list(APPEND PROJECT_WARNING_FLAGS
-    -Weverything # Enables every Clang warning.
+    -Weverything                       # Enables every Clang warning.
+    -Wno-c++98-compat                  # This project is not compatible with C++98.
+    -Wno-c++98-compat-pedantic         # This project is not compatible with C++98.
     )
 elseif (CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
   # Warnings present on all supported versions of GCC and Clang.
   list(APPEND PROJECT_WARNING_FLAGS
-    -Wall                         # Enable most warning messages.
-    -Wextra                       # Print extra warnings.
-    -pedantic                     # Issue warnings needed for strict compliance to the standard.
-    -Wcast-align                  # Warn about pointer casts which increase alignment.
-    -Wconversion                  # Warn for implicit type conversions that may change a value.
-    -Wformat=2                    # Warn about printf/scanf/strftime/strfmon format string anomalies.
-    -Wnon-virtual-dtor            # Warn about non-virtual destructors.
-    -Wold-style-cast              # Warn if a C-style cast is used in a program.
-    -Woverloaded-virtual          # Warn about overloaded virtual function names.
-    -Wsign-conversion             # Warn for implicit conversions between signed and unsigned integers.
-    -Wshadow                      # Warn when one variable shadows another.
-    -Wunused                      # Enable all -Wunused- warnings.
+    -Wall                # Enables most warnings.
+    -Wextra              # Enables an extra set of warnings.
+    -pedantic            # Strict compliance to the standard is not met.
+    -Wcast-align         # Pointer casts which increase alignment.
+    -Wcast-qual          # A pointer is cast to remove a type qualifier, or add an unsafe one.
+    -Wconversion         # Implicit type conversions that may change a value.
+    -Wformat=2           # printf/scanf/strftime/strfmon format string anomalies.
+    -Wnon-virtual-dtor   # Non-virtual destructors are found.
+    -Wold-style-cast     # C-style cast is used in a program.
+    -Woverloaded-virtual # Overloaded virtual function names.
+    -Wsign-conversion    # Implicit conversions between signed and unsigned integers.
+    -Wshadow             # One variable shadows another.
+    -Wswitch-enum        # A switch statement has an index of enumerated type and lacks a case.
+    -Wundef              # An undefined identifier is evaluated in an #if directive.
+    -Wunused             # Enable all -Wunused- warnings.
     )
   # Enable additional warnings depending on the compiler and compiler version in use.
   if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
     list(APPEND PROJECT_WARNING_FLAGS
-      -Wlogical-op                # Warn when a logical operator is always evaluating to true or false.
+      -Wdisabled-optimization       # GCC’s optimizers are unable to handle the code effectively.
+      -Weffc++                      # Warnings related to guidelines from Scott Meyers’ Effective C++ books.
+      -Wlogical-op                  # Warn when a logical operator is always evaluating to true or false.
+      -Wsign-promo                  # Overload resolution chooses a promotion from unsigned to a signed type.
+      -Wswitch-default              # A switch statement does not have a default case.
+      -Wredundant-decls             # Something is declared more than once in the same scope.
       )
     if (NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 4.6)
       list(APPEND PROJECT_WARNING_FLAGS
-        -Wdouble-promotion        # Warn about implicit conversions from "float" to "double".
+        -Wdouble-promotion          # Warn about implicit conversions from "float" to "double".
         )
     endif ()
     if (NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 4.8)
       list(APPEND PROJECT_WARNING_FLAGS
-        -Wuseless-cast            # Warn about useless casts.
+        -Wuseless-cast              # Warn about useless casts.
+        )
+    endif ()
+    if (NOT (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 5))
+      list(APPEND PROJECT_WARNING_FLAGS
+        -Wdate-time                 # Warn when encountering macros that might prevent bit-wise-identical compilations.
+        -Wsuggest-final-methods     # Virtual methods that could be declared final or in an anonymous namespace.
+        -Wsuggest-final-types       # Types with virtual methods that can be declared final or in an anonymous namespace.
+        -Wsuggest-override          # Overriding virtual functions that are not marked with the override keyword.
         )
     endif ()
     if (NOT (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 6))
       list(APPEND PROJECT_WARNING_FLAGS
-        -Wduplicated-cond         # Warn about duplicated conditions in an if-else-if chain.
-        -Wmisleading-indentation  # Warn when indentation does not reflect the block structure.
-        -Wnull-dereference        # Dereferencing a pointer may lead to erroneous or undefined behavior.
+        -Wduplicated-cond           # Warn about duplicated conditions in an if-else-if chain.
+        -Wmisleading-indentation    # Warn when indentation does not reflect the block structure.
+        -Wmultiple-inheritance      # Do not allow multiple inheritance.
+        -Wnull-dereference          # Dereferencing a pointer may lead to undefined behavior.
         )
     endif ()
     if (NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 7)
       list(APPEND PROJECT_WARNING_FLAGS
-        -Wduplicated-branches     # Warn about duplicated branches in if-else statements.
+        -Walloca                    # Warn on any usage of alloca in the code.
+        -Wduplicated-branches       # Warn about duplicated branches in if-else statements.
+        )
+    endif ()
+    if (NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 8)
+      list(APPEND PROJECT_WARNING_FLAGS
+        -Wextra-semi                # Redundant semicolons after in-class function definitions.
+        -Wunsafe-loop-optimizations # The loop cannot be optimized because the compiler cannot assume anything.
+        )
+    endif ()
+    if (NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 10)
+      list(APPEND PROJECT_WARNING_FLAGS
+        -Warith-conversion          # Stricter implicit conversion warnings in arithmetic operations.
+        -Wredundant-tags            # Redundant class-key and enum-key where it can be eliminated.
         )
     endif ()
   elseif (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
     list(APPEND PROJECT_WARNING_FLAGS
-      -Wdouble-promotion          # Warn about implicit conversions from "float" to "double".
-      -Wnull-dereference          # Dereferencing a pointer may lead to erroneous or undefined behavior.
-      -Wno-unknown-warning-option # Ignore unknown warning options.
+      -Wdouble-promotion            # Warn about implicit conversions from "float" to "double".
+      -Wnull-dereference            # Dereferencing a pointer may lead to erroneous or undefined behavior.
+      -Wno-unknown-warning-option   # Ignore unknown warning options.
       )
   endif ()
 elseif (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
